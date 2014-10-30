2d7a77436cf2c4e623b68b5f3df919bdb1aef121
Troy Siedsma <tsiedsma@lithiumhosting.com>
2014-10-30 09:28:47 -0500

    Added support for ipv6
    Changed the way the IP is cached to use a hex of the IP which is generated from a new method called ipToHex
    Also added the FILTER_FLAG_IPV6 to the validateIp method.
    Also added geoplugin_cached to the response for easily checking if the IP is in the cache
    Finally, modified the 404 response detection to check if the geoplugin_status property isset in the response.

--- src/Fuhrmann/LarageoPlugin/LarageoPlugin.php
+++ src/Fuhrmann/LarageoPlugin/LarageoPlugin.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Cache;
 class LarageoPlugin {
 
     /**
+     * Is the current IP stored in the cache?
+     * @var bool
+     */
+    public $isCached = false;
+    /**
      * The URL of the geoPlugin API (json).
      *
      * @var string
@@ -21,15 +26,21 @@ class LarageoPlugin {
         if ($ip == NULL) $ip = $this->getIpAdress();
 
         $url = str_replace('{IP}', $ip, $this->api_adress);
-        $subnet = substr($ip, 0, strrpos ($ip, '.'));
-
+        $hex = $this->ipToHex($ip);
         $me = $this;
 
+        // Check if the IP is in the cache
+        if (Cache::has($hex))
+        {
+            $this->isCached = true;
+        }
         // Use the IP info stored in cache or store it
-        $ipInfo = Cache::remember($subnet, 10080, function() use ($me, $url)
+        $ipInfo = Cache::remember($hex, 10080, function() use ($me, $url)
         {
             return $me->fetchInfo($url);
         });
+
+        $ipInfo->geoplugin_cached = $this->isCached;
 
         return $ipInfo;
     }
@@ -62,7 +73,7 @@ class LarageoPlugin {
      */
     protected function validateIp($ip)
     {
-        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE | FILTER_FLAG_IPV6) === false) {
             return false;
         }
         return true;
@@ -99,11 +110,97 @@ class LarageoPlugin {
             throw new \Exception("Ops! The data is empty! Is " . $url . ' acessible?');
         }
 
-        if($response->geoplugin_status == 404)
+        if (isset($response->geoplugin_status) && $response->geoplugin_status == 404)
         {
             throw new \Exception("Ops! Your request returned a 404 error! Is " . $url . ' acessible?');
         }
 
         return $response;
+    }
+
+    /**
+     * Return a hex string of the current IP.  Used as the key for cache storage
+     *
+     * @param $ipAddress
+     *
+     * @return bool|string
+     */
+    public function ipToHex($ipAddress)
+    {
+        $hex = '';
+        if (strpos($ipAddress, ',') !== false)
+        {
+            $splitIp = explode(',', $ipAddress);
+            $ipAddress = trim($splitIp[0]);
+        }
+        $isIpV6 = false;
+        $isIpV4 = false;
+        if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false)
+        {
+            $isIpV6 = true;
+        } else if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false)
+        {
+            $isIpV4 = true;
+        }
+        if (! $isIpV4 && ! $isIpV6)
+        {
+            return false;
+        }
+        // IPv4 format
+        if ($isIpV4)
+        {
+            $parts = explode('.', $ipAddress);
+            for ($i = 0; $i < 4; $i ++)
+            {
+                $parts[ $i ] = str_pad(dechex($parts[ $i ]), 2, '0', STR_PAD_LEFT);
+            }
+            $ipAddress = '::' . $parts[0] . $parts[1] . ':' . $parts[2] . $parts[3];
+            $hex = join('', $parts);
+        } // IPv6 format
+        else
+        {
+            $parts = explode(':', $ipAddress);
+            // If this is mixed IPv6/IPv4, convert end to IPv6 value
+            if (filter_var($parts[ count($parts) - 1 ], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false)
+            {
+                $partsV4 = explode('.', $parts[ count($parts) - 1 ]);
+                for ($i = 0; $i < 4; $i ++)
+                {
+                    $partsV4[ $i ] = str_pad(dechex($partsV4[ $i ]), 2, '0', STR_PAD_LEFT);
+                }
+                $parts[ count($parts) - 1 ] = $partsV4[0] . $partsV4[1];
+                $parts[] = $partsV4[2] . $partsV4[3];
+            }
+            $numMissing = 8 - count($parts);
+            $expandedParts = array();
+            $expansionDone = false;
+            foreach ($parts as $part)
+            {
+                if (! $expansionDone && $part == '')
+                {
+                    for ($i = 0; $i <= $numMissing; $i ++)
+                    {
+                        $expandedParts[] = '0000';
+                    }
+                    $expansionDone = true;
+                } else
+                {
+                    $expandedParts[] = $part;
+                }
+            }
+            foreach ($expandedParts as &$part)
+            {
+                $part = str_pad($part, 4, '0', STR_PAD_LEFT);
+            }
+            $ipAddress = join(':', $expandedParts);
+            $hex = join('', $expandedParts);
+        }
+        // Validate the final IP
+        if (! filter_var($ipAddress, FILTER_VALIDATE_IP))
+        {
+            return false;
+        }
+
+        return strtolower(str_pad($hex, 32, '0', STR_PAD_LEFT));
     }
 }
