f9fa5776577a60c1ff924d014b2e6a9907c47f57
Ricardo Fuhrmann <fuhrmanns@gmail.com>
2014-10-30 12:00:38 -0300

    Fix tabs

--- src/Fuhrmann/LarageoPlugin/ServiceProvider.php
+++ src/Fuhrmann/LarageoPlugin/ServiceProvider.php
@@ -4,32 +4,32 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('fuhrmann/larageo-plugin');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('fuhrmann/larageo-plugin');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $this->app->singleton('larageo_plugin', function() {
             return new LarageoPlugin;
         });
-	}
+    }
 }
